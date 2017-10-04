8904092fb956d538cf6e51c907427dfeb60fe992
gabrielcleiria@gmail.com <gabrielcleiria@gmail.com>
2017-10-04 16:10:09 -0300

    Tratamento dos valor inseridos no formulário OK

--- manipula_form.php
+++ manipula_form.php
@@ -1,7 +1,6 @@
 <?php
 include 'conexao.php';
 
-
 $nome = $_POST['nome'];
 $cpf = $_POST['cpf'];
 $rua = $_POST['rua'];
@@ -20,42 +19,8 @@ $valor_md = $_POST['valor_md'];
 $forma_pgto_md = $_POST['forma_pgto_md'];
 $email_cliente = $_POST['email_cliente'];
 
-/*$insere = "insert into formulario(nome, cpf, rua, numero_casa, cidade, rep_legal, cpf_rl, dia_vencimento, primeira_parcela,
-valor_insc, forma_pgto_insc, num_parcelas, valor_parcelas, forma_pgto_parc, valor_md, forma_pgto_md, email_cliente) values
+$insere = "insert into formulario (nome, cpf, rua, numero_casa, cidade, rep_legal, cpf_rl, dia_vencimento, primeira_parcela, valor_insc, forma_pgto_insc, num_parcelas, valor_parcelas, forma_pgto_parc, valor_md, forma_pgto_md, email_cliente) values ('".$nome."', '".$cpf."', '".$rua."','".$numero_casa."', '".$cidade."', '".$rep_legal."', '".$cpf_rl."', '".$dia_vencimento."', '".$primeira_parcela."', '".$valor_insc."', '".$forma_pgto_insc."', '".$num_parcelas."', '".$valor_parcelas."', '".$forma_pgto_parc."', '".$valor_md."', '".$forma_pgto_md."', '".$email_cliente."')";
 
-('".$nome."','".$cpf."','".$rua."','".$numero_casa."','".$cidade."','".$rep_legal."','".$cpf_rl."','".$dia_vencimento."',
-'".$primeira_parcela."','".$valor_insc."','".$forma_pgto_insc."','".$num_parcelas."','".$valor_parcelas."','".$forma_pgto_parc."',
-'".$valor_md."','".$forma_pgto_md."','".$email_cliente."',)";
-
-if(mysqli_query($conexao, $insere)){
-	echo "cadastrado";
-}else{
-	echo "erro";
-}
-
-$executa = mysqli_query($conexao, $insere);*/
-
-/*$insere = "insert into formulario(nome, cpf, rua, numero_casa, cidade, rep_legal, cpf_rl, dia_vencimento, primeira_parcela, valor_insc, forma_pgto_insc, num_parcelas, valor_parcelas, forma_pgto_parc, valor_md, forma_pgto_md, email_cliente) values 
-('".$nome."','".$cpf."','".$rua."','".$numero_casa."','".$cidade."','".$rep_legal."','".$cpf_rl."','".$dia_vencimento."','".$primeira_parcela."','".$valor_insc."','".$forma_pgto_insc."','".$num_parcelas."','".$valor_parcelas."','".$forma_pgto_parc."','".$valor_md."','".$forma_pgto_md."','".$email_cliente."',)";
-
-$teste = mysqli_query($conexao, $insere);*/
-
-echo("Nome: ".$nome."<br>".
-	"cpf: ".$cpf."<br>".
-	"rua: ".$rua."<br>".
-	"numero Casa: ".$numero_casa."<br>".
-	"cidade: ".$cidade."<br>".
-	"rep_legal: ".$rep_legal."<br>".
-	"cpf_rl: ".$cpf_rl."<br>".
-	"dia_vencimento: ".$dia_vencimento."<br>".
-	"primeira_parcela: ".$primeira_parcela."<br>".
-	"valor_insc: ".$valor_insc."<br>".
-	"forma_pgto_insc: ".$forma_pgto_insc."<br>".
-	"num_parcelas: ".$num_parcelas."<br>".
-	"valor_parcelas: ".$valor_parcelas."<br>".
-	"forma_pgto_parc: ".$forma_pgto_parc."<br>".
-	"valor_md: ".$valor_md."<br>".
-	"forma_pgto_md: ".$forma_pgto_md."<br>".
-	"email_cliente: ".$email_cliente."<br>");
+$executa = mysqli_query($conexao, $insere);
 
 ?>
