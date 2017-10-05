cc91fd6689c5d2ce18c93ee4ad04b7e2e56c94f2
gabrielcleiria@gmail.com <gabrielcleiria@gmail.com>
2017-10-05 15:55:31 -0300

    pagina inicial

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<title>Catálogo de Serviços</title>
+
+
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+<link rel="stylesheet" href="assets/animate/animate.css" />
+<link rel="stylesheet" href="assets/animate/set.css" />
+<link rel="stylesheet" href="assets/gallery/blueimp-gallery.min.css">
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+
+<link rel="stylesheet" href="assets/style.css">
+
+</head>
+
+<body>
+<div class="topbar animated fadeInLeftBig"></div>
+
+
+<div class="navbar-wrapper">
+      <div class="container">
+
+        <div class="navbar navbar-default navbar-fixed-top" role="navigation" id="top-nav">
+          <div class="container">
+            <div class="navbar-header">
+              
+              <a class="navbar-brand" href="#sobre"><h2>Empresa X</h2></a>
+             
+
+
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+
+            </div>
+            
+            <div class="navbar-collapse  collapse">
+              <ul class="nav navbar-nav navbar-right scroll">
+                <li class="active"><a href="#sobre">Sobre</a></li>
+                 <li ><a href="#works">Catálogo de serviços</a></li>
+                 <li ><a href="#contato">Contato</a></li>
+              </ul>
+            </div>
+            
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div id="sobre"  class="container spacer sobre">
+<h2 class="text-center wowload fadeInUp">Empresa de prestação de serviços na área de TI</h2>  
+  <div class="row">
+
+  <div class="col-sm-6 wowload fadeInLeft">
+    <h4><i class="fa fa-building"></i> Como trabalhamos?</h4>
+    <p>Ao serem abertos chamados para serviços recebemos a notificação e verificamos a disponibilidade de funcionários
+     de acordo com a categoria do chamado e a quantidade de funcionários necessária para atender o chamado.</p>
+    </div>
+
+  <div class="col-sm-6 wowload fadeInRight">
+  <h4><i class="fa fa-money"></i> O que usamos para basear nossos preços?</h4>
+  <p>Nossos valores são definidos utilizando alguns parâmetros, como a complexidade do serviço, o número de profissionais necessários para realizar o serviço e o preço por hora cobrado pelo serviço.</p>    
+  </div>
+
+  </div>
+
+  <div class="process">
+  <h3 class="text-center wowload fadeInUp">Áreas de atuação</h3>
+  <ul class="row text-center list-inline  wowload bounceInUp">
+      <li>
+            <span><i class="fa fa-history"></i><b>Informações</b></span>
+        </li>
+        <li>
+            <span><i class="fa fa-puzzle-piece"></i><b>Desenvolvimento</b></span>
+        </li>
+        <li>
+            <span><i class="fa fa-share-alt"></i><b>Redes</b></span>
+        </li>
+        <li>
+            <span><i class="fa fa-magic"></i><b>Manutenção</b></span>
+        </li>        
+        <li>
+            <span><i class="fa fa-cloud-upload"></i><b>Armazenamento</b></span>
+        </li>
+    </ul>
+  </div>
+</div>
+
+<center><h2>Catálogo de Serviços</h2></center>
+
+<div id="works"  class="clearfix grid"> 
+    <figure class="effect-oscar  wowload fadeIn">
+        <img src="img1.png" alt="Banco de dados"/>
+        <figcaption>
+            <h2>Data Base</h2>
+            <p>Manutenção e atualização de banco de dados<br>
+            <a >R$80,00/h</a></p>            
+        </figcaption>
+    </figure>
+
+    <figure class="effect-oscar  wowload fadeIn">
+        <img src="img2.jpg" alt="Desenvolvimento web"/>
+        <figcaption>
+            <h2>Web</h2>
+            <p>Desenvolvimento para plataformas web<br>
+            <a >R$40,00/h</a></p>            
+        </figcaption>
+    </figure>
+
+    <figure class="effect-oscar  wowload fadeIn">
+        <img src="img3.png" alt="Desenvolvimento desktop"/>
+        <figcaption>
+            <h2>Desktop</h2>
+            <p>Desenvolvimento para plataformas Desktop<br>
+            <a >R$50,00/h</a></p>            
+        </figcaption>
+    </figure>
+
+    <figure class="effect-oscar  wowload fadeIn">
+        <img src="img4.jpg" alt="Redes"/>
+        <figcaption>
+            <h2>Redes</h2>
+            <p>Manutenção e instalação de redes<br>
+            <a >R$30,00/h</a></p>            
+        </figcaption>
+    </figure>
+
+      <figure class="effect-oscar  wowload fadeIn">
+        <img src="img5.png" alt="Suporte técnico"/>
+        <figcaption>
+            <h2>Suporte</h2>
+            <p>Suporte técnico para computadores e notebooks<br>
+            <a >R$70,00</a></p>            
+        </figcaption>
+    </figure>
+
+
+    <figure class="effect-oscar  wowload fadeIn">
+        <img src="img6.png" alt="Dúvidas e informações"/>
+        <figcaption>
+            <h2>Informações</h2>
+            <p>Informações e resolução de dúvidas<br>
+            <a >R$10,00/h</a></p>            
+        </figcaption>
+    </figure>         
+</div>
+
+<div class="highlight-info">
+<div class="overlay spacer">
+<div class="container">
+<div class="row text-center  wowload fadeInDownBig">
+  <div class="col-sm-3 col-xs-6">
+  <i class="fa fa-smile-o  fa-5x"></i><h4>245 Clientes</h4>
+  </div>
+  <div class="col-sm-3 col-xs-6">
+  <i class="fa fa-rocket  fa-5x"></i><h4>275 Projetos</h4>
+  </div>
+  <div class="col-sm-3 col-xs-6">
+  <i class="fa fa-cloud-upload fa-5x"></i><h4>158 Serviços Hospedados</h4>
+  </div>
+  <div class="col-sm-3 col-xs-6">
+  <i class="fa fa-map-marker fa-5x"></i><h4>35 Profissionais</h4>
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+<div id="contato" class="spacer">
+<div class="container contactform center">
+<h2 class="text-center  wowload fadeInUp">Para mais informações entre em contato</h2>
+  <div class="row wowload fadeInLeftBig">      
+      <div class="col-sm-6 col-sm-offset-3 col-xs-12">      
+        <input type="text" placeholder="Nome">
+        <input type="text" placeholder="E-mail">
+        <textarea rows="5" placeholder="Menssagem"></textarea>
+        <button class="btn btn-primary" onClick="alert('E-mail enviado com sucesso!')"><i class="fa fa-paper-plane"></i>Enviar</button>
+      </div>
+  </div>
+</div>
+</div>
+
+<div class="footer text-center spacer">
+Redes Sociais que usamos:<br><br>
+<p class="wowload flipInX"><a href="#"><i class="fa fa-facebook fa-2x"></i></a><a href="#"><i class="fa fa-instagram fa-2x"></i></a> <a href="#"><i class="fa fa-twitter fa-2x"></i></a></p>
+Copyright 2017 - Empresa X
+</div>
+
+<a href="#sobre" class="gototop "><i class="fa fa-angle-up  fa-3x"></i></a>
+
+
+<script src="assets/jquery.js"></script>
+<script src="assets/wow/wow.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.js" type="text/javascript" ></script>
+<script src="assets/mobile/touchSwipe.min.js"></script>
+<script src="assets/respond/respond.js"></script>
+<script src="assets/gallery/jquery.blueimp-gallery.min.js"></script>
+<script src="assets/script.js"></script>
+
+</body>
+</html>
