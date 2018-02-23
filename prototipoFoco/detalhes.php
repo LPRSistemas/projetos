65e98c17ac4cfa99da61fafbd61249543c13bb8e
gabrielcleiria@gmail.com <gabrielcleiria@gmail.com>
2018-02-23 17:03:18 -0300

    att

---
+++ prototipoFoco/detalhes.php
@@ -0,0 +1,149 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Detalhes</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700|Roboto:400,300,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/animate.css">
+
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<link rel="stylesheet" href="css/icomoon.css">
+
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	
+
+	</head>
+	<body>
+	
+	<div id="fh5co-header">
+	<div class="container">
+	
+		<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+			<div id="fh5co-logo">
+				<a href="index.php">FOCO</a>
+			</div>	
+	</div>
+</div>
+
+	<div id="fh5co-main">
+		<div class="fh5co-overlay-mobile"></div>
+		<div id="fh5co-home" class="js-fullheight" data-section="home">
+
+			<div class="flexslider">
+				
+				<div class="fh5co-overlay"></div>
+				<div class="fh5co-text">
+					<div class="container">
+						<div class="row text-center">
+							<h1 class="animate-box">Detalhes do curso</h1>
+							<div class="fh5co-go animate-box">
+								<a href="#" class="js-fh5co-next">
+									Navegar
+									<span><i class="icon-arrow-down2"></i></span>
+								</a>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			  	<ul class="slides">
+			   	<li style="background-image: url(6.jpg);" data-stellar-background-ratio="0.5"></li>
+			  	</ul>
+
+			</div>
+		</div>
+
+		<div id="fh5co-content">
+			<div class="container">
+				<div class="row r-pb">
+					<div class="col-md-8 col-md-offset-2 text-center section-heading">
+						<h2 class="fh5co-section-title animate-box">Informações</h2>
+						<p class="fh5co-lead animate-box">Curso com X horas de duração, onde serão tratados temas como...</p>
+					</div>
+				</div>
+
+				<div class="row">
+					<figure class="animate-box">
+						<img src="images/slide_3.jpg" alt="Free HTML5 Template by FREEHTML5.co" class="img-responsive">
+					</figure>
+					<figure class="animate-box">
+						<img src="images/slide_2.jpg" alt="Free HTML5 Template by FREEHTML5.co" class="img-responsive">
+					</figure>
+					<figure class="animate-box">
+						<img src="images/slide_1.jpg" alt="Free HTML5 Template by FREEHTML5.co" class="img-responsive">
+					</figure>
+					<figure class="animate-box">
+						<img src="images/pic_13.jpg" alt="Free HTML5 Template by FREEHTML5.co" class="img-responsive">
+					</figure>
+					<figure class="animate-box">
+						<img src="images/pic_14.jpg" alt="Free HTML5 Template by FREEHTML5.co" class="img-responsive">
+					</figure>
+				</div>
+	
+					<h2 class="fh5co-section-title animate-box"><a href="#">Realizar pré-matricula</a></h2>
+					<p class="fh5co-lead animate-box">Valor do curso<br>Formas de pagamento...</p>
+
+			</div>
+		</div>
+
+		<div id="fh5co-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-sm-6 animate-box">
+						<div class="fh5co-footer-widget">
+							<p></p>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6 animate-box">
+						<div class="fh5co-footer-widget">
+							<ul class="fh5co-social pull-right">
+								<li>
+									<a href="#"><i class="icon-twitter"></i></a>
+								</li>
+								<li>
+									<a href="#"><i class="icon-facebook"></i></a>
+								</li>
+								<li>
+									<a href="#"><i class="icon-google"></i></a>
+								</li>
+							</ul>
+						<div class="fh5co-footer-widget">
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Main JS -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
