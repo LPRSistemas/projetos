198aad67a70da6a6064546887087dcdc55bb08d9
gabrielcleiria@gmail.com <gabrielcleiria@gmail.com>
2018-02-23 17:02:20 -0300

    Informações mais reais na página

--- prototipoFoco/index.php
+++ prototipoFoco/index.php
@@ -21,6 +21,8 @@
 
 	<link rel="stylesheet" href="css/style.css">
 
+	<link rel="stylesheet" href="style.css">
+
 
 	<!-- Modernizr JS -->
 	<script src="js/modernizr-2.6.2.min.js"></script>
@@ -67,8 +69,8 @@
 			<div class="container">
 				<div class="row r-pb">
 					<div class="col-md-8 col-md-offset-2 text-center section-heading">
-						<h2 class="fh5co-section-title animate-box">Portfolio</h2>
-						<p class="fh5co-lead animate-box">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+						<h2 class="fh5co-section-title animate-box">Cursos</h2>
+						<p class="fh5co-lead animate-box">Abaixo os cursos que oferecemos, clique para mais informações.</p>
 					</div>
 				</div>
 				<div class="row">
@@ -216,16 +218,16 @@
 			<div class="container">
 				<div class="row r-pb">
 					<div class="col-md-8 col-md-offset-2 text-center section-heading animate-box">
-						<h2 class="fh5co-section-title">About</h2>
-						<p class="fh5co-lead">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+						<h2 class="fh5co-section-title">Nossa equipe</h2>
+						<p class="fh5co-lead">Nossa equipe de professores...</p>
 					</div>
 				</div>
 				<div class="row">
 					<div class="col-md-6 animate-box">
-						<div class="fh5co-text with-plus">
-							<h2>Rob Smith</h2>
-							<span class="fh5co-meta">Photographer, Video Editor</span>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+						<div class="fh5co-text">
+							<h2>Alex</h2>
+							<span class="fh5co-meta">Matemática</span>
+							<p>Formado em matemática...</p>
 						</div>
 					</div>					
 				</div>
@@ -238,14 +240,45 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-8 col-md-offset-2 text-center section-heading">
-						<h2 class="fh5co-section-title animate-box">Contact</h2>
-						<p class="fh5co-lead animate-box">We'd love to talk about your story.</p>
-						<p class="animate-box">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-						<p class="animate-box"><a href="#" class="btn btn-primary btn-lg">Get in touch</a></p>
+						<h1 class="fh5co-section-title animate-box">Contato</h1>
+						<p class="fh5co-lead animate-box">Entre em contato preenchendo o formulário.</p>
+					</div>
+
+					<div class="col-md-8 col-md-offset-2">
+				<form name="formulario">
+                    <div class="form-group animate-box float-label-control">
+                        <label>Nome</label>
+                        <input type="text" class="form-control" placeholder="Seu nome">
+                    </div>
+                    <div class="form-group animate-box float-label-control">
+                        <label>E-mail</label>
+                        <input type="email" class="form-control" placeholder="E-mail para contato">
+                    </div>
+                    <div class="form-group animate-box float-label-control">
+                        <label>Assunto</label>
+                        <input type="text" class="form-control" placeholder="Assunto da mensagem">
+                    </div>
+                    <div class="form-group animate-box float-label-control">
+                        <label>Mensagem</label>
+                        <textarea class="form-control" placeholder="Digite aqui sua mensagem" rows="5"></textarea>
+                    </div>
+
+                <p class="animate-box">
+                    <input type="button" class="btn btn-primary btn-lg" onClick="limpa()" value="Limpar formulário">
+					<input type="button" class="btn btn-primary btn-lg" value="Enviar">
+				</p>	
+
+                </form>	                					
 					</div>
 				</div>
 			</div>
 		</div>
+
+<script type="text/javascript">
+		function limpa(){
+		formulario.reset();
+	}
+</script>
 
 		<div id="fh5co-testimony" style="background-image: url(images/slide_3.jpg);" data-stellar-background-ratio="0.5">
 			<div class="fh5co-overlay"></div>
@@ -255,17 +288,17 @@
 							<ul class="slides">
 								<li>
 									<blockquote>
-										<p>&ldquo;I think design would be better if designers were much more skeptical about its applications. If you believe in the potency of your craft, where you choose to dole it out is not something to take lightly.&rdquo; <cite>&mdash; Frank Chimero</cite></p>
+										<p>NOTÍCIA 1</p>
 									</blockquote>
 								</li>
 								<li>
 									<blockquote>
-										<p>&ldquo;Design must be functional and functionality must be translated into visual aesthetics, without any reliance on gimmicks that have to be explained.&rdquo; <cite>&mdash; Ferdinand A. Porsche</cite></p>
+										<p>NOTÍCIA 2</p>
 									</blockquote>
 								</li>
 								<li>
 									<blockquote>
-										<p>&ldquo;Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn’t really do it, they just saw something. It seemed obvious to them after a while.&rdquo; <cite>&mdash; Steve Jobs</cite></p>
+										<p>NOTÍCIA 3</p>
 									</blockquote>
 								</li>
 								
@@ -296,16 +329,7 @@
 									<a href="#"><i class="icon-facebook"></i></a>
 								</li>
 								<li>
-									<a href="#"><i class="icon-vine"></i></a>
-								</li>
-								<li>
-									<a href="#"><i class="icon-behance"></i></a>
-								</li>
-								<li>
 									<a href="#"><i class="icon-google"></i></a>
-								</li>
-								<li>
-									<a href="#"><i class="icon-vimeo"></i></a>
 								</li>
 							</ul>
 						<div class="fh5co-footer-widget">
@@ -314,22 +338,15 @@
 			</div>
 		</div>
 
-	</div> <!-- END fh5co-page -->
+	</div>
 
-	<!-- jQuery -->
-	<script src="js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
+		<script src="js/jquery.min.js"></script>
 	<script src="js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
 	<script src="js/bootstrap.min.js"></script>
-	<!-- Waypoints -->
 	<script src="js/jquery.waypoints.min.js"></script>
-	<!-- Stellar Parallax -->
 	<script src="js/jquery.stellar.min.js"></script>
-	<!-- Flexslider -->
-	<script src="js/jquery.flexslider-min.js"></script>
-	<!-- Main JS -->
-	<script src="js/main.js"></script>
+		<script src="js/jquery.flexslider-min.js"></script>
+		<script src="js/main.js"></script>
 
 	</body>
 </html>
